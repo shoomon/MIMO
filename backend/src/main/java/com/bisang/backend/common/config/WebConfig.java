1250ccbbcafdb999a95b55ca5dc90844f7a78f70
PaikMyeongGyu <qoraudrb123@gmail.com>
2025-01-28 17:18:28 +0900

    feat: 기본 config 설정 추가

---
+++ backend/src/main/java/com/bisang/backend/common/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.bisang.backend.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
