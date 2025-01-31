66f92643f05fc77efb8294df6f8f3935a505ac76
PaikMyeongGyu <qoraudrb123@gmail.com>
2025-01-31 15:42:04 +0900

    feat: TeamUser 엔티티 추가

---
+++ backend/src/main/java/com/bisang/backend/team/domain/TeamUser.java
@@ -0,0 +1,72 @@
+package com.bisang.backend.team.domain;
+
+import static jakarta.persistence.EnumType.STRING;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.NoArgsConstructor;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = PROTECTED)
+@Table(
+        name = "team_user",
+        uniqueConstraints = {
+            @UniqueConstraint(name = "UK_team_user", columnNames = {"team_id", "user_id"})
+        }
+)
+public class TeamUser {
+
+    @Id @Column(name = "team_user_id")
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private Long teamId;
+
+    @Column(length = 30, nullable = false, unique = true)
+    private String nickname;
+
+    @Column(name = "team_user_role", nullable = false)
+    @Enumerated(STRING)
+    private TeamUserRole role;
+
+    @Column(name = "nofitication_status", nullable = false)
+    @Enumerated(STRING)
+    private TeamNotificationStatus status;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    private TeamUser(
+            Long userId,
+            Long teamId,
+            String nickname,
+            TeamUserRole role,
+            TeamNotificationStatus status
+    ) {
+        this.userId = userId;
+        this.teamId = teamId;
+        this.nickname = nickname;
+        this.role = role;
+        this.status = status;
+    }
+}
