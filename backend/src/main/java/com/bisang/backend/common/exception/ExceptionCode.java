2aefe3801b566964034349f12ae634d2efc802ad
PaikMyeongGyu <qoraudrb123@gmail.com>
2025-01-31 18:04:49 +0900

    feat: 계좌, 트랜잭션 엔티티 추가

--- backend/src/main/java/com/bisang/backend/common/exception/ExceptionCode.java
+++ backend/src/main/java/com/bisang/backend/common/exception/ExceptionCode.java
@@ -14,7 +14,9 @@ public enum ExceptionCode {
     UNAUTHORIZED_ACCESS(3000, "접근할 수 없는 리소스입니다."),
     INVALID_REFRESH_TOKEN(3001, "유효하지 않은 Refresh Token입니다."),
     FAILED_TO_VALIDATE_TOKEN(3002, "토큰 검증에 실패했습니다."),
-    INVALID_ACCESS_TOKEN(3003, "유효하지 않은 Access Token입니다.");
+    INVALID_ACCESS_TOKEN(3003, "유효하지 않은 Access Token입니다."),
+
+    NOT_ENOUGH_MONEY(4000, "잔고 잔액이 부족합니다.");
 
     private final int code;
     private final String message;
