f7ddc78eb28607baa07140a827e781dab6228b09
PaikMyeongGyu <qoraudrb123@gmail.com>
2025-02-03 13:03:37 +0900

    feat: 회원 관련 ArgumentResolver 및 회원 정보 수정 추가

--- backend/src/main/java/com/bisang/backend/user/controller/UserController.java
+++ backend/src/main/java/com/bisang/backend/user/controller/UserController.java
@@ -1,5 +1,10 @@
 package com.bisang.backend.user.controller;
 
+import com.bisang.backend.auth.annotation.AuthUser;
+import com.bisang.backend.user.domain.User;
+import com.bisang.backend.user.domain.request.UpdateNameRequest;
+import com.bisang.backend.user.domain.request.UpdateNicknameRequest;
+import com.bisang.backend.user.domain.request.UpdateProfileUriRequest;
 import com.bisang.backend.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -11,4 +16,30 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final UserService userService;
 
+    @PatchMapping("/nickname")
+    public ResponseEntity<Void> updateNickname(
+        @AuthUser User user,
+        UpdateNicknameRequest request
+    ) {
+        userService.updateNickname(user, request.nickname());
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/name")
+    public ResponseEntity<Void> updateName(
+        @AuthUser User user,
+        UpdateNameRequest request
+    ) {
+        userService.updateName(user, request.name());
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/profileUri")
+    public ResponseEntity<Void> updateNickname(
+        @AuthUser User user,
+        UpdateProfileUriRequest request
+    ) {
+        userService.updateProfileUri(user, request.profileUri());
+        return ResponseEntity.ok().build();
+    }
 }
--- backend/src/main/java/com/bisang/backend/user/domain/request/UpdateProfileUriRequest.java
+++ backend/src/main/java/com/bisang/backend/user/domain/request/UpdateProfileUriRequest.java
@@ -1,6 +1,6 @@
 package com.bisang.backend.user.domain.request;
 
-public record UpdateProfileUri(
+public record UpdateProfileUriRequest(
     String profileUri
 ) {
 }
