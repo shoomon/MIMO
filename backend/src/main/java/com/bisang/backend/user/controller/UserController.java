61379f16cdeb5904ab3fd70d31f0ad79f72d0884
PaikMyeongGyu <qoraudrb123@gmail.com>
2025-02-03 12:39:49 +0900

    feat: User 이름, 닉네임, 프로필 이미지 로직 추가

---
+++ backend/src/main/java/com/bisang/backend/user/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.bisang.backend.user.controller;
+
+import com.bisang.backend.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+}
---
+++ backend/src/main/java/com/bisang/backend/user/domain/request/UpdateNicknameRequest.java
@@ -0,0 +1,6 @@
+package com.bisang.backend.user.domain.request;
+
+public record UpdateNicknameRequest(
+    String nickname
+) {
+}
