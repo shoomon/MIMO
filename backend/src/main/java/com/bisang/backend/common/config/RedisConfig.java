2752f42056e6220c13a3e1aa1870498802999e7f
PaikMyeongGyu <qoraudrb123@gmail.com>
2025-02-03 17:23:06 +0900

    chore: redisConfig 편집

--- backend/src/main/java/com/bisang/backend/common/config/RedisConfig.java
+++ backend/src/main/java/com/bisang/backend/common/config/RedisConfig.java
@@ -1,20 +1,34 @@
 package com.bisang.backend.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+    @Value("${spring.auth.redis-uri}")
+    private String redisUri;
+
+    @Value("${spring.auth.redis-port}")
+    private Integer redisPort;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisUri, redisPort);
+    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer()); // 값도 String 직렬화
         template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new StringRedisSerializer()); // 해시 값 직렬화 추가
         return template;
     }
 }
