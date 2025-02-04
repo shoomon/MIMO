d9c27593fd48e8765bfcdc80c7a37d7a1fb22ad2
jump6746 <jump6746@naver.com>
2025-02-04 16:13:30 +0900

    [markup] : [S12P11A504] Nav Level 구현
      - useLocation을 사용해서 현재 경로에서 breadcrumb 구현

--- frontend/src/hooks/useNav.ts
+++ frontend/src/hooks/useNav.ts
@@ -1,3 +1,4 @@
+import { ROUTER_CONFIG } from "@/constants/navItems";
 import { useLocation } from "react-router-dom";
 
 export const useNavActive = (link:string) => {
@@ -6,3 +7,20 @@ export const useNavActive = (link:string) => {
 
     return location.pathname.includes(link);
 }
+
+export const useBreadcrumbs = () => {
+
+    const location = useLocation();
+    const segments = location.pathname.split("/").filter(Boolean);
+
+    const filteredSegments = segments.filter(segment => isNaN(Number(segment)));
+    
+    const breadcrumbs = filteredSegments.map(segment => ({
+        to: `/${segment}`,
+        label: ROUTER_CONFIG[segment]?.name || segment
+    }));
+
+    return breadcrumbs;
+}
+
+
