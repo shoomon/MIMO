1530f316025e2db5c7f8eb44ae98aa27aa6ecb24
윤다은 <syoon4486@gmail.com>
2025-02-04 17:57:37 +0900

    comment: ChatType 주석 추가

--- backend/src/main/java/com/bisang/backend/chat/domain/ChatType.java
+++ backend/src/main/java/com/bisang/backend/chat/domain/ChatType.java
@@ -1,5 +1,10 @@
 package com.bisang.backend.chat.domain;
 
 public enum ChatType {
+    /**
+     * ENTER : 누군가의 입장(가입)을 알리는 메시지
+     * MESSAGE : 일반 채팅
+     * LEAVE : 누군가의 탈퇴(강퇴)를 알리는 메시지
+     */
     ENTER, MESSAGE, LEAVE
 }
