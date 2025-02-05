6d763ae68f0d0c7dfc9f93c2359053c81f79a03d
서주원 <tjwndnjs7@naver.com>
2025-02-06 01:02:29 +0900

    [markup][S12P11A504-94]MemberList 컴포넌트 구현 -sprite svg 추가

--- frontend/src/utils/dateParsing.ts
+++ frontend/src/utils/dateParsing.ts
@@ -1,11 +1,19 @@
-const dateParsing = (targetDate: Date) => {
-    return (
+const dateParsing = (targetDate: Date, onlyDate: boolean = false) => {
+    const dateStr =
         targetDate.getFullYear() +
         '년 ' +
         (targetDate.getMonth() + 1) +
         '월 ' +
         targetDate.getDate() +
-        '일 ' +
+        '일';
+
+    if (onlyDate) {
+        return dateStr;
+    }
+
+    return (
+        dateStr +
+        ' ' +
         targetDate.getHours() +
         '시 ' +
         targetDate.getMinutes() +
