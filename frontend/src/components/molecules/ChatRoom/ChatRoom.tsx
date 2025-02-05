cfc4bb71dc38e6515b4caacdcfc3cddff77359a7
jump6746 <jump6746@naver.com>
2025-02-06 00:35:28 +0900

    [markup] : [S12P11A504-95] ChatRoom 구현중
    - 디자인에 맞춰 구현
    - 글자 parsing 기능 추가해야함
    
    -

---
+++ frontend/src/components/molecules/ChatRoom/ChatRoom.tsx
@@ -0,0 +1,7 @@
+import ChatRoomView, { ChatRoomProps } from './ChatRoom.view';
+
+const ChatRoom = (props: ChatRoomProps) => {
+    return <ChatRoomView {...props} />;
+};
+
+export default ChatRoom;
