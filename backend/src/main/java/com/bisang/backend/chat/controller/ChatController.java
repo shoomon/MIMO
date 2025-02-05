f46c21889820df80df7c0a7cc263dc200e5edca4
윤다은 <syoon4486@gmail.com>
2025-02-05 17:38:54 +0900

    feat: 채팅방 멤버 추가 삭제 로직 추가, 사용자 인증 로직 추가 (아직 테스트용)

--- backend/src/main/java/com/bisang/backend/chat/controller/ChatController.java
+++ backend/src/main/java/com/bisang/backend/chat/controller/ChatController.java
@@ -2,40 +2,80 @@ package com.bisang.backend.chat.controller;
 
 import com.bisang.backend.chat.domain.ChatType;
 import com.bisang.backend.chat.domain.redis.RedisChatMessage;
-import com.bisang.backend.chat.domain.request.ChatMessageRequest;
-import com.bisang.backend.chat.domain.response.ChatMessageResponse;
+import com.bisang.backend.chat.controller.request.ChatMessageRequest;
+import com.bisang.backend.chat.controller.response.ChatMessageResponse;
 import com.bisang.backend.chat.service.ChatService;
+import com.bisang.backend.common.exception.ChatAccessInvalidException;
+import com.bisang.backend.common.exception.ExceptionCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
+@RequestMapping("/chat")
 @RequiredArgsConstructor
 public class ChatController {
 
     private final ChatService chatService;
 
-    @MessageMapping("/chat/{roomId}")
+    @MessageMapping("/{roomId}")
     public void sendMessage(@DestinationVariable Long roomId, ChatMessageRequest chat) {
-        if (chatService.isMember(chat.teamUserId(), roomId)) {
+        //TODO: SimpleUser? 어노테이션으로 userId 가져와서 아래 if문에 teamUserId를 userId로 바꿔야함
+        Long userId = 1L;
+        if (chatService.isMember(roomId, userId, chat.teamUserId())) {
           RedisChatMessage redisMessage = new RedisChatMessage(chat.teamUserId(), chat.chat(), LocalDateTime.now(), ChatType.MESSAGE);
 
           chatService.broadcastMessage(roomId, redisMessage);
         }
     }
 
-    @GetMapping("/messages/{roomId}")
-    public ResponseEntity<List<ChatMessageResponse>> getMessages(@PathVariable Long roomId) {
-        //TODO: 사용자가 해당 팀에 포함된 유저인지 확인 필요
+    @GetMapping("/messages/{roomId}/{teamUserId}")
+    public ResponseEntity<List<ChatMessageResponse>> getMessages(
+//            @AuthUser User user,
+            @PathVariable("roomId") Long roomId,
+            @PathVariable("teamUserId") Long teamUserId
+    ) {
+        if (chatService.isMember(roomId, 1L, teamUserId)) {
+            List<ChatMessageResponse> list = chatService.getMessages(roomId);
+            return ResponseEntity.ok().body(list);
+        }
 
-        List<ChatMessageResponse> list = chatService.getMessages(roomId);
-        return ResponseEntity.ok().body(list);
+        throw new ChatAccessInvalidException(ExceptionCode.UNAUTHORIZED_ACCESS);
+    }
+
+
+
+
+    //테스트용
+    @PostMapping("/test/{roomId}")
+    public ResponseEntity<String> sendM(@PathVariable Long roomId, @RequestBody ChatMessageRequest chat) {
+        if (chatService.isMember(roomId, 1L, chat.teamUserId())) {
+            RedisChatMessage redisMessage = new RedisChatMessage(chat.teamUserId(), chat.chat(), LocalDateTime.now(), ChatType.MESSAGE);
+
+            chatService.broadcastMessage(roomId, redisMessage);
+            return ResponseEntity.ok().body("전송 성공");
+        }
+
+        return ResponseEntity.badRequest().body("전송 실패");
+    }
+
+    @GetMapping("/test/enterChatroom")
+    public ResponseEntity<String> enterChatroom(@RequestParam("userId") Long userId, @RequestParam("nickname") String nickname, @RequestParam("teamId") Long teamId) {
+        chatService.enterChatroom(teamId, userId, nickname);
+        return ResponseEntity.ok().body(nickname + "님 입장");
+    }
+
+    @GetMapping("/test/leaveChatroom")
+    public ResponseEntity<String> leaveChatroom(@RequestParam("userId") Long userId, @RequestParam("teamId") Long teamId) {
+        if (chatService.leaveChatroom(userId, teamId)) {
+            return ResponseEntity.ok().body(userId + "번 유저 퇴장");
+        }
+        return ResponseEntity.badRequest().body("퇴장 실패");
     }
 }
--- backend/src/main/java/com/bisang/backend/chat/repository/ChatRepository.java
+++ backend/src/main/java/com/bisang/backend/chat/repository/ChatRepository.java
@@ -1,10 +1,13 @@
 package com.bisang.backend.chat.repository;
 
+import com.bisang.backend.chat.domain.ChatMessage;
+import com.bisang.backend.chat.domain.ChatroomUser;
 import com.bisang.backend.chat.domain.redis.RedisChatMessage;
-import com.bisang.backend.chat.domain.response.ChatMessageResponse;
+import com.bisang.backend.chat.domain.redis.RedisTeamMember;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -14,14 +17,20 @@ import java.util.Set;
 public class ChatRepository {
     private final ChatRedisRepository chatRedisRepository;
     private final ChatroomUserJpaRepository chatroomUserJpaRepository;
+    private final ChatMessageJpaRepository chatMessageJpaRepository;
     private final RedisCacheRepository redisCacheRepository;
 
-    public void redisInsertMemberUser(long teamId, long teamUserId) {
-        chatRedisRepository.insertMember(teamId, teamUserId);
+    public void insertRedisMemberUser(long teamId, RedisTeamMember teamMember) {
+        chatRedisRepository.insertMember(teamId, teamMember);
     }
 
-    public void redisRemoveMember(long teamId, long teamUserId) {
-        chatRedisRepository.deleteMember(teamId, teamUserId);
+    public void insertJpaMemberUser(ChatroomUser chatroomUser) {
+        chatroomUserJpaRepository.save(chatroomUser);
+    }
+
+    public void removeMember(long teamId, RedisTeamMember teamMember) {
+        chatRedisRepository.deleteMember(teamId, teamMember);
+        chatroomUserJpaRepository.deleteById(teamMember.getTeamUserId());
     }
 
     public Set<Long> getTeamMembers(long teamId) {
@@ -32,27 +41,36 @@ public class ChatRepository {
         return teamMembers;
     }
 
-    public void redisUpdateUserChatroom(long teamUserId, long teamId, Double timestamp) {
-        chatRedisRepository.updateUserChatroom(teamUserId, teamId, timestamp);
+    public void redisUpdateUserChatroom(long userId, long teamId, Double timestamp) {
+        chatRedisRepository.updateUserChatroom(userId, teamId, timestamp);
     }
 
-    public void redisDeleteUserChatroom(long teamUserId, long teamId) {
-        chatRedisRepository.deleteUserChatroom(teamUserId, teamId);
+    public void redisDeleteUserChatroom(long userId, long teamId) {
+        chatRedisRepository.deleteUserChatroom(userId, teamId);
     }
 
     public void redisSaveMessage(long teamId, RedisChatMessage message) {
         chatRedisRepository.saveMessage(teamId, message);
     }
 
-    public boolean isMember(Long teamUserId, Long teamId) {
-        if (chatRedisRepository.isMember(teamUserId, teamId)) {
+    public boolean isMember(Long teamId, RedisTeamMember teamMember) {
+        if (chatRedisRepository.isMember(teamId, teamMember)) {
+            System.out.println("redis true");
             return true;
         }
-        return chatroomUserJpaRepository.existsByIdAndChatroomId(teamUserId, teamId);
+
+        if (chatroomUserJpaRepository.existsByIdAndUserIdAndChatroomId(teamMember.getTeamUserId(), teamMember.getUserId(), teamId)) {
+            System.out.println("jpa true");
+            chatRedisRepository.insertMember(teamId, teamMember);
+            return true;
+        }
+
+        return false;
     }
 
     public Map<Object, Object> getUserInfo(Long teamUserId, Long userId) {
         Map<Object, Object> userInfo = redisCacheRepository.getUserProfile(teamUserId);
+        System.out.println(teamUserId + ":" + userInfo.get("name"));
         if (userInfo.isEmpty()) {
             String nickname = chatroomUserJpaRepository.findNicknameById(teamUserId);
             //TODO: 유저쪽에서 프로필 이미지 받아오는 메서드 받아와서 넣어야함
@@ -67,17 +85,27 @@ public class ChatRepository {
         return userInfo;
     }
 
-    public List<ChatMessageResponse> getMessages(Long roomId) {
-        List<ChatMessageResponse> messageList = chatRedisRepository.getMessages(roomId);
-        if (messageList == null || messageList.isEmpty()) {
-            //TODO: db에서 최대 100개 가져온다.
-            return null;
-        }
+    public Long getTeamUserId(Long userId, Long teamId) {
+        return chatroomUserJpaRepository.findTeamUserIdByUserIdAndChatroomId(userId, teamId);
+    }
 
-        if (messageList.size() < 100) {
-            //TODO: db에서 부족한 수만큼 채워서 가져온다.
-        }
+    public List<RedisChatMessage> getMessages(Long roomId) {
+        List<RedisChatMessage> messageList = chatRedisRepository.getMessages(roomId);
+        int size = messageList.size();
 
+        if (size < 100) {
+            List<RedisChatMessage> newMessageList = getMessagesFromDB(100-size, roomId);
+            newMessageList.addAll(messageList);
+            return newMessageList;
+        }
         return messageList;
+    }
+
+    private List<RedisChatMessage> getMessagesFromDB(int size, Long roomId) {
+        List<ChatMessage> messages = chatMessageJpaRepository.findTop100ByChatroomIdOrderByIdDesc(roomId);
+        List<RedisChatMessage> result = new LinkedList<>();
+        //TODO: 가져와서 RedisChatMessage로 매핑해서 result에 넣어주기
+
+        return result;
     }
 }
