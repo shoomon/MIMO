87ec51b54fc336afba7711d5d968926723fb2576
PaikMyeongGyu <qoraudrb123@gmail.com>
2025-02-06 17:23:06 +0900

    feat: S3Upload 속도 제한 기능 추가

---
+++ backend/src/main/java/com/bisang/backend/common/annotation/S3Limiter.java
@@ -0,0 +1,12 @@
+package com.bisang.backend.common.annotation;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Target({METHOD})
+@Retention(RUNTIME)
+public @interface S3Limiter {
+}
