dfa6f4edfe1ebbc8094570f3a32558c98d7596b9
kyoungbeom <pkb1998@naver.com>
2025-02-06 11:21:27 +0900

    chore: 설명 주석 변경 및 추가

--- backend/src/main/java/com/bisang/backend/account/balance/converter/TransactionConverter.java
+++ backend/src/main/java/com/bisang/backend/account/balance/converter/TransactionConverter.java
@@ -1,7 +1,7 @@
 package com.bisang.backend.account.balance.converter;
 
 import com.bisang.backend.account.balance.domain.Transaction;
-import com.bisang.backend.account.balance.charge.controller.request.PaymentResultRequest;
+import com.bisang.backend.account.balance.controller.request.PaymentResultRequest;
 import com.bisang.backend.account.balance.domain.TransactionCategory;
 import com.bisang.backend.account.balance.domain.TransactionStatus;
 import jakarta.persistence.Converter;
@@ -9,8 +9,8 @@ import jakarta.persistence.Converter;
 @Converter
 public class TransactionConverter {
 
-    // 포인트 충전
-    public static Transaction PaymentResultRequestToPointTransaction(PaymentResultRequest paymentResultRequest) {
+    // 잔액 충전 트랜잭션
+    public static Transaction PaymentResultRequestToTransaction(PaymentResultRequest paymentResultRequest) {
         return Transaction.builder()
                 .balance(paymentResultRequest.getPaidAmount())
                 .senderAccountNumber(null)
