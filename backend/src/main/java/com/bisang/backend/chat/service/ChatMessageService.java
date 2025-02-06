462d2892271a3f733e89948c4492a306b74ecadc
윤다은 <syoon4486@gmail.com>
2025-02-06 15:20:03 +0900

    Refractor: 서비스, 컨트롤러 도메인별로 분리

---
+++ backend/src/main/java/com/bisang/backend/chat/service/ChatMessageService.java
@@ -0,0 +1,70 @@
+package com.bisang.backend.chat.service;
+
+import com.bisang.backend.chat.controller.response.ChatMessageResponse;
+import com.bisang.backend.chat.domain.redis.RedisChatMessage;
+import com.bisang.backend.chat.repository.ChatRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.ZoneOffset;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class ChatMessageService {
+
+    private final ChatRepository repository;
+    private final SimpMessagingTemplate template;
+
+    public void broadcastMessage(Long teamId, RedisChatMessage message) {
+        Set<Long> teamMembers = repository.getTeamMembers(teamId);
+
+        //여기에 teamMember 없으면 log 찍어야하나? 없을 수 없는 곳인데 없는 경우임
+
+        for (Long userId : teamMembers) {
+            repository.redisUpdateUserChatroom(
+                    userId,
+                    teamId,
+                    (double)message.getTimestamp().toInstant(ZoneOffset.ofTotalSeconds(0)).toEpochMilli());
+        }
+        repository.redisSaveMessage(teamId, message);
+
+        Map<Object, Object> userInfo = repository.getUserInfo(message.getTeamUserId(), message.getUserId());
+        ChatMessageResponse messageResponse = new ChatMessageResponse(
+                message.getId(),
+                message.getTeamUserId(),
+                (String)userInfo.get("name"),
+                (String)userInfo.get("profileImage"),
+                message.getChat(),
+                message.getTimestamp(),
+                message.getType()
+        );
+
+        //template.convertAndSend(messageResponse);
+    }
+
+    public List<ChatMessageResponse> getMessages(Long roomId, Long messageId) {
+        List<RedisChatMessage> messageList = repository.getMessages(roomId, messageId);
+        List<ChatMessageResponse> responseList = new LinkedList<>();
+
+        for (RedisChatMessage message : messageList) {
+            Map<Object, Object> userInfo = repository.getUserInfo(message.getTeamUserId(), message.getUserId());
+            ChatMessageResponse messageResponse = new ChatMessageResponse(
+                    message.getId(),
+                    message.getTeamUserId(),
+                    (String)userInfo.get("name"),
+                    (String)userInfo.get("profileImage"),
+                    message.getChat(),
+                    message.getTimestamp(),
+                    message.getType()
+            );
+            responseList.add(messageResponse);
+        }
+
+        return responseList;
+    }
+}
