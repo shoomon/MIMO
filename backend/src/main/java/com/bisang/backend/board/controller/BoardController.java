2476ec000d821e3f7c695494ff95ef8022ab6ca4
shoomon <kate010117@naver.com>
2025-02-13 10:11:07 +0900

    refactor: 게시글 수정 put 매핑으로 변경
    기존 patch -> put으로 변경, update post request 추가되는 파일 multipart로 받토록 수정
    
    25.02.13

--- backend/src/main/java/com/bisang/backend/board/controller/BoardController.java
+++ backend/src/main/java/com/bisang/backend/board/controller/BoardController.java
@@ -9,8 +9,8 @@ import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -91,7 +91,7 @@ public class BoardController {
     }
 
     @Transactional
-    @PatchMapping
+    @PutMapping
     public ResponseEntity<String> updatePost(
             @AuthUser User user,
             @RequestParam(value = "post", required = true) Long postId,
--- backend/src/main/java/com/bisang/backend/board/controller/request/UpdatePostRequest.java
+++ backend/src/main/java/com/bisang/backend/board/controller/request/UpdatePostRequest.java
@@ -3,11 +3,12 @@ package com.bisang.backend.board.controller.request;
 import java.util.List;
 
 import com.bisang.backend.board.controller.dto.BoardFileDto;
+import org.springframework.web.multipart.MultipartFile;
 
 public record UpdatePostRequest(
         String title,
         String description,
-        List<BoardFileDto> filesToDelete,
+        List<MultipartFile> filesToDelete,
         List<BoardFileDto> filesToAdd
 ) {
 }
