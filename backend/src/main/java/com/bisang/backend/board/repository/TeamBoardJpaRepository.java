093299e87ee4628f77e7ac0dbf59ca228468ea8b
shoomon <kate010117@naver.com>
2025-02-17 10:45:51 +0900

    fix: 게시판 순서 오류 진짜 해결
    쿼리에 order by를 다른데 붙이고 있었다
    
    25.02.17

--- backend/src/main/java/com/bisang/backend/board/repository/TeamBoardJpaRepository.java
+++ backend/src/main/java/com/bisang/backend/board/repository/TeamBoardJpaRepository.java
@@ -12,9 +12,9 @@ public interface TeamBoardJpaRepository extends JpaRepository<TeamBoard, Long> {
     @Query("SELECT b.boardName FROM TeamBoard b WHERE b.id = :teamBoardId")
     String getTeamBoardNameById(@Param("teamBoardId") Long teamBoardId);
 
-    @Query("SELECT b FROM TeamBoard b WHERE b.teamId = :teamBoardId")
+    @Query("SELECT b FROM TeamBoard b WHERE b.teamId = :teamBoardId ORDER BY b.id asc")
     List<TeamBoard> findAllByTeamId(@Param("teamBoardId") Long teamId);
 
-    @Query("SELECT b.id FROM TeamBoard b WHERE b.teamId = :teamBoardId ORDER BY b.id asc")
+    @Query("SELECT b.id FROM TeamBoard b WHERE b.teamId = :teamBoardId")
     List<Long> getTeamBoardIdByTeamId(@Param("teamBoardId") Long teamId);
 }
