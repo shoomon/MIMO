37180e5320414d5070fb2b0d708457a06770ac6a
shoomon <kate010117@naver.com>
2025-02-17 16:59:20 +0900

    fix: 댓글 달려는 게시글이 없는 경우 예외처리 추가
    댓글 서비스
    
    25.02.17

--- backend/src/main/java/com/bisang/backend/common/exception/ExceptionCode.java
+++ backend/src/main/java/com/bisang/backend/common/exception/ExceptionCode.java
@@ -50,7 +50,8 @@ public enum ExceptionCode {
     NOT_AUTHOR(7001, "작성자만 글을 수정할 수 있습니다."),
     PAGE_LIMIT(7002, "최대 페이지 수를 초과할 수 없습니다."),
     BOARD_LIMIT(7003, "게시판의 최대 게시글 수를 초과했습니다."),
-    BOARD_NOT_FOUNT(7004, "게시글을 찾을 수 없습니다.");
+    BOARD_NOT_FOUNT(7004, "게시글을 찾을 수 없습니다."),
+    COMMENT_NOT_FOUNT(7005, "댓글을 찾을 수 없습니다.");
 
     private final int code;
     private final String message;
