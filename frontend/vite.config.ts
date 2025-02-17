7c246f654f67fadc1dcd3359e8c5d10b28c46e91
jump6746 <jump6746@naver.com>
2025-02-17 19:44:33 +0900

    [feat]: openvidu 중간 커밋

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+    server: {
+        host:true,
+        port:5173
+    },
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
