12a7d73912925790c1a2faf8351e127a79fe60d1
PaikMyeongGyu <qoraudrb123@gmail.com>
2025-02-18 12:41:22 +0900

    refactor: TeamSearchQuerydslRepository로 TeamQuerydslRepository 일부 이동

---
+++ backend/src/main/java/com/bisang/backend/team/repository/TeamSearchQuerydslRepository.java
@@ -0,0 +1,241 @@
+package com.bisang.backend.team.repository;
+
+import com.bisang.backend.team.controller.dto.*;
+import com.bisang.backend.team.domain.TeamTag;
+import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static com.bisang.backend.common.utils.PageUtils.SHORT_PAGE_SIZE;
+import static com.bisang.backend.team.domain.QTag.tag;
+import static com.bisang.backend.team.domain.QTeam.team;
+import static com.bisang.backend.team.domain.QTeamDescription.teamDescription;
+import static com.bisang.backend.team.domain.QTeamReview.teamReview;
+import static com.bisang.backend.team.domain.QTeamTag.teamTag;
+import static com.bisang.backend.team.domain.QTeamUser.teamUser;
+import static java.util.Collections.emptyList;
+import static java.util.Comparator.comparing;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.*;
+
+@Repository
+@RequiredArgsConstructor
+public class TeamSearchQuerydslRepository {
+    private final TeamUserJpaRepository teamUserJpaRepository;
+    private final JPAQueryFactory queryFactory;
+    private final TeamJpaRepository teamJpaRepository;
+
+    public Optional<TeamTag> findByName(Long teamId, String name) {
+        return Optional.ofNullable(queryFactory
+                .select(teamTag)
+                .from(teamTag).join(tag).on(teamTag.tagId.eq(tag.id))
+                .where(tag.name.eq(name), teamTag.teamId.eq(teamId)).fetchOne());
+    }
+
+    public List<TagDto> searchTags(String searchText, Integer pageNumber) {
+        if (pageNumber == null) {
+            pageNumber = 1;
+        }
+
+        return queryFactory
+            .select(Projections.constructor(TagDto.class,
+                tag.id,
+                tag.name))
+            .from(tag)
+            .where(tag.name.contains(searchText))
+            .orderBy(tag.name.desc())
+            .limit(SHORT_PAGE_SIZE)
+            .offset((pageNumber - 1) * SHORT_PAGE_SIZE)
+            .fetch();
+    }
+
+    public Long searchTagsCount(String searchText) {
+        return queryFactory
+            .select(tag.count())
+            .from(tag)
+            .where(tag.name.contains(searchText))
+            .fetchOne();
+    }
+
+    public Long searchTeamsCount(Long tagId) {
+        return queryFactory
+            .select(teamTag.teamId.count())
+            .from(teamTag)
+            .where(teamTag.tagId.eq(tagId))
+            .fetchOne();
+    }
+
+    public List<SimpleTeamDto> searchTeams(Long tagId, Integer pageNumber) {
+        if (pageNumber == null) {
+            pageNumber = 1;
+        }
+
+        List<Long> teamIds = queryFactory
+            .select(teamTag.teamId)
+            .from(teamTag)
+            .where(teamTag.tagId.eq(tagId))
+            .orderBy(teamTag.teamId.desc())
+            .limit(SHORT_PAGE_SIZE)
+            .offset((pageNumber - 1) * SHORT_PAGE_SIZE)
+            .fetch();
+
+        List<SimpleTeamDto> teams = queryFactory
+            .select(Projections.constructor(SimpleTeamDto.class,
+                team.id,
+                team.name,
+                team.shortDescription,
+                team.teamProfileUri,
+                Expressions.numberTemplate(Double.class, "{0}", 0.0),
+                Expressions.numberTemplate(Long.class, "{0}", 0L),
+                team.maxCapacity,
+                JPAExpressions.select(teamUser.count())
+                    .from(teamUser)
+                    .where(teamUser.teamId.eq(team.id)),
+                Expressions.constant(emptyList())
+            ))
+            .from(team)
+            .where(team.id.in(teamIds))
+            .orderBy(team.id.desc())
+            .fetch();
+
+        Map<Long, List<String>> tagsMap = getTagsByTeamIds(teamIds);
+        Map<Long, SimpleTeamReviewDto> teamReviews = getReviewsByTeamIds(teamIds);
+
+        return teams.stream()
+            .map(teamDto -> {
+                List<String> tags = tagsMap.getOrDefault(teamDto.teamId(), emptyList());
+                SimpleTeamReviewDto simpleTeamReview
+                    = teamReviews.getOrDefault(
+                    teamDto.teamId(),
+                    new SimpleTeamReviewDto(teamDto.teamId(), 0D, 0L));
+                return createSimpleDto(teamDto, tags, simpleTeamReview);
+            })
+            .sorted(comparing(SimpleTeamDto::teamId).reversed())
+            .toList();
+    }
+
+    public Long searchTeamsCount(String searchText) {
+        return queryFactory
+                .select(team.count())
+                .from(team)
+                .leftJoin(teamDescription).on(team.id.eq(teamDescription.id))
+                .where(team.name.contains(searchText)
+                        .or(teamDescription.description.contains(searchText)))
+                .fetchOne();
+    }
+
+    public List<SimpleTeamDto> searchTeams(String searchText, Integer pageNumber) {
+        if (pageNumber == null) {
+            pageNumber = 1;
+        }
+
+        List<SimpleTeamDto> teams = queryFactory
+                .select(Projections.constructor(SimpleTeamDto.class,
+                        team.id,
+                        team.name,
+                        team.shortDescription,
+                        team.teamProfileUri,
+                        Expressions.numberTemplate(Double.class, "{0}", 0.0),
+                        Expressions.numberTemplate(Long.class, "{0}", 0L),
+                        team.maxCapacity,
+                        JPAExpressions.select(teamUser.count())
+                                .from(teamUser)
+                                .where(teamUser.teamId.eq(team.id)),
+                        Expressions.constant(emptyList())
+                        ))
+                .from(team)
+                .leftJoin(teamDescription).on(team.id.eq(teamDescription.id))
+                .where(team.name.contains(searchText)
+                        .or(teamDescription.description.contains(searchText)))
+                .limit(SHORT_PAGE_SIZE)
+                .offset((pageNumber - 1) * SHORT_PAGE_SIZE)
+                .fetch();
+
+        List<Long> teamIds = teams.stream()
+                .map(SimpleTeamDto::teamId)
+                .toList();
+        Map<Long, List<String>> tagsMap = getTagsByTeamIds(teamIds);
+        Map<Long, SimpleTeamReviewDto> teamReviews = getReviewsByTeamIds(teamIds);
+
+        return teams.stream()
+                .map(teamDto -> {
+                    List<String> tags = tagsMap.getOrDefault(teamDto.teamId(), emptyList());
+                    SimpleTeamReviewDto simpleTeamReview
+                            = teamReviews.getOrDefault(
+                            teamDto.teamId(),
+                            new SimpleTeamReviewDto(teamDto.teamId(), 0D, 0L));
+                    return createSimpleDto(teamDto, tags, simpleTeamReview);
+                })
+                .sorted(comparing(SimpleTeamDto::teamId).reversed())
+                .toList();
+    }
+
+    private Map<Long, SimpleTeamReviewDto> getReviewsByTeamIds(List<Long> teamIds) {
+        List<SimpleTeamReviewDto> teamReviews = queryFactory
+            .select(Projections.constructor(SimpleTeamReviewDto.class,
+                teamReview.teamId,
+                teamReview.score.avg(),
+                teamReview.count()
+            ))
+            .from(teamReview)
+            .where(teamReview.teamId.in(teamIds))
+            .groupBy(teamReview.teamId)
+            .fetch();
+
+        return teamReviews.stream()
+            .collect(toMap(
+                SimpleTeamReviewDto::teamId,
+                identity()
+            ));
+    }
+
+    private Map<Long, List<String>> getTagsByTeamIds(List<Long> teamIds) {
+        List<Tuple> results = queryFactory
+            .select(teamTag.teamId, tag.name)
+            .from(teamTag)
+            .join(tag).on(teamTag.tagId.eq(tag.id))
+            .where(teamTag.teamId.in(teamIds))
+            .fetch();
+
+        return results.stream()
+            .collect(groupingBy(
+                tuple -> tuple.get(teamTag.teamId),
+                mapping(tuple -> tuple.get(tag.name), toList())
+            ));
+    }
+
+    private SimpleTeamDto createSimpleDto(SimpleTeamDto dto, List<String> tags, SimpleTeamReviewDto simpleTeamReview) {
+        return new SimpleTeamDto(
+            dto.teamId(),
+            dto.name(),
+            dto.description(),
+            dto.teamProfileUri(),
+            simpleTeamReview.reviewScore(),
+            simpleTeamReview.reviewCount(),
+            dto.maxCapacity(),
+            dto.currentCapacity(),
+            tags
+        );
+    }
+
+    private SimpleTeamReviewDto getSimpleTeamReview(Long teamId) {
+        return queryFactory
+            .select(Projections.constructor(SimpleTeamReviewDto.class,
+                teamReview.teamId,
+                teamReview.score.avg(),
+                teamReview.count()
+            ))
+            .from(teamReview)
+            .where(teamReview.teamId.eq(teamId))
+            .groupBy(teamReview.teamId)
+            .fetchOne();
+    }
+}
--- backend/src/main/java/com/bisang/backend/team/service/TeamSearchService.java
+++ backend/src/main/java/com/bisang/backend/team/service/TeamSearchService.java
@@ -6,7 +6,7 @@ import com.bisang.backend.team.controller.dto.TagDto;
 import com.bisang.backend.team.controller.response.TeamTagResponse;
 import com.bisang.backend.team.controller.response.TeamTagSearchResponse;
 import com.bisang.backend.team.controller.response.TeamTitleDescSearchResponse;
-import com.bisang.backend.team.repository.TeamQuerydslRepository;
+import com.bisang.backend.team.repository.TeamSearchQuerydslRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,29 +16,29 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class TeamSearchService {
-    private final TeamQuerydslRepository teamQuerydslRepository;
+    private final TeamSearchQuerydslRepository teamSearchQuerydslRepository;
 
     @EveryOne
     @Transactional(readOnly = true)
     public TeamTitleDescSearchResponse getTeamsByTitleOrDescription(String searchKeyword, Integer pageNumber) {
-        List<SimpleTeamDto> teams = teamQuerydslRepository.searchTeams(searchKeyword, pageNumber);
-        Long numberOfTeams = teamQuerydslRepository.searchTeamsCount(searchKeyword);
+        List<SimpleTeamDto> teams = teamSearchQuerydslRepository.searchTeams(searchKeyword, pageNumber);
+        Long numberOfTeams = teamSearchQuerydslRepository.searchTeamsCount(searchKeyword);
         return new TeamTitleDescSearchResponse(numberOfTeams.intValue(), pageNumber, teams.size(), teams);
     }
 
     @EveryOne
     @Transactional(readOnly = true)
     public TeamTagSearchResponse getTeamsByTag(Long tagId, Integer pageNumber) {
-        List<SimpleTeamDto> teams = teamQuerydslRepository.searchTeams(tagId, pageNumber);
-        Long teamsCount = teamQuerydslRepository.searchTeamsCount(tagId);
+        List<SimpleTeamDto> teams = teamSearchQuerydslRepository.searchTeams(tagId, pageNumber);
+        Long teamsCount = teamSearchQuerydslRepository.searchTeamsCount(tagId);
         return new TeamTagSearchResponse(teamsCount.intValue(), pageNumber, teams.size(), teams);
     }
 
     @EveryOne
     @Transactional(readOnly = true)
     public TeamTagResponse getTagBySearchKeyword(String searchKeyword, Integer pageNumber) {
-        List<TagDto> tags = teamQuerydslRepository.searchTags(searchKeyword, pageNumber);
-        Long numberOfTags = teamQuerydslRepository.searchTagsCount(searchKeyword);
+        List<TagDto> tags = teamSearchQuerydslRepository.searchTags(searchKeyword, pageNumber);
+        Long numberOfTags = teamSearchQuerydslRepository.searchTagsCount(searchKeyword);
         return new TeamTagResponse(numberOfTags.intValue(), pageNumber, tags.size(), tags);
     }
 
