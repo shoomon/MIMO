23c92aad3abdc10631a7c65d6f37600e07251b29
PaikMyeongGyu <qoraudrb123@gmail.com>
2025-02-19 22:45:08 +0900

    fix: my-info review 정보 추가 hasReview

--- backend/src/main/java/com/bisang/backend/team/repository/TeamReviewJpaRepository.java
+++ backend/src/main/java/com/bisang/backend/team/repository/TeamReviewJpaRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.bisang.backend.team.domain.TeamReview;
 
 public interface TeamReviewJpaRepository extends JpaRepository<TeamReview, Long> {
+    Boolean existsTeamReviewByTeamUserId(Long teamUserId);
 }
