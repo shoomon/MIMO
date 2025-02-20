8be338a1475e71e08f077b1e6dd953c784e4df64
PaikMyeongGyu <qoraudrb123@gmail.com>
2025-02-20 17:48:21 +0900

    fix: 띄어쓰기 허용 양쪽 빈칸 제거

--- backend/src/main/java/com/bisang/backend/team/domain/Team.java
+++ backend/src/main/java/com/bisang/backend/team/domain/Team.java
@@ -117,6 +117,10 @@ public class Team {
         this.maxCapacity = maxCapacity;
         this.teamLeaderId = teamLeaderId;
         this.teamChatroomId = teamChatroomId;
+        name = name.trim();
+        String pattern = "^[a-zA-Z0-9가-힣 ]{1,30}$";
+        Validate.matchesPattern(name, pattern,
+            "모임 이름은 영문, 숫자, 한글로만 구성되어 있으며, 길이는 1자리 이상 30자리 이하이어야 합니다.");
         this.name = name;
         int shortDescriptionLength = min(description.getDescription().length(), 97);
         this.shortDescription = description.getDescription().substring(0, shortDescriptionLength) + "...";
@@ -130,7 +134,8 @@ public class Team {
     }
 
     public void updateTeamName(String name) {
-        String pattern = "^[a-zA-Z0-9가-힣]{1,30}$";
+        name = name.trim();
+        String pattern = "^[a-zA-Z0-9가-힣 ]{1,30}$";
         Validate.matchesPattern(name, pattern,
                 "모임 이름은 영문, 숫자, 한글로만 구성되어 있으며, 길이는 1자리 이상 30자리 이하이어야 합니다.");
         this.name = name;
