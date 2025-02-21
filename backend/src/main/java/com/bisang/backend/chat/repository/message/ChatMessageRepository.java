9a022970ffbe69c2d2d004572c1cb1160c4a0257
윤다은 <syoon4486@gmail.com>
2025-02-21 10:09:20 +0900

    fix: DB 들어간 후 가져오기 LocalDateTime -> Long으로 type 맞춤

--- backend/src/main/java/com/bisang/backend/chat/repository/message/ChatMessageRepository.java
+++ backend/src/main/java/com/bisang/backend/chat/repository/message/ChatMessageRepository.java
@@ -1,5 +1,6 @@
 package com.bisang.backend.chat.repository.message;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Collections;
@@ -62,7 +63,7 @@ public class ChatMessageRepository {
 
         int limit = Math.min(size, messages.size());
 
-        for (int i = limit - 1; i >= 0; i--) {
+        for (int i = 0; i < limit; i++) {
             ChatMessage chatMessage = messages.get(i);
 
             LocalDateTime createdAt = chatMessage.getCreatedAt();
@@ -103,11 +104,13 @@ public class ChatMessageRepository {
         if (chatMessage == null) {
             log.error("메시지가 하나도 존재하지 않습니다. db 문제");
             result.put("lastChat", "임시 채팅");
-            result.put("lastDatetime", LocalDateTime.now());
+            result.put("lastDatetime", Instant.now().toEpochMilli());
             return;
         }
         result.put("lastChat", chatMessage.getMessage());
-        result.put("lastDatetime", chatMessage.getCreatedAt());
+
+        Long createdAt = chatMessage.getCreatedAt().atZone(ZoneId.of("UTC")).toInstant().toEpochMilli();
+        result.put("lastDatetime", createdAt);
     }
 
     public Long calculateUnreadCount(Long chatroomId, Double lastReadScore, Long lastChatId) {
